26a853654bdb854fc7a4c9d4e4cd6b5cb949e7cf
teamsheikify-bit <teamsheikify@gmail.com>
2026-07-26 14:43:35 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// PWA installability ke liye basic fetch handler zaroori hai
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
